49e6d8da41ec68a56dcf8ad61f53b1b21122bc2b
Oswaldinho <oswalfut_96@hotmail.com>
2017-08-25 14:48:25 -0500

    congratulations

--- src/components/products/ProductDetail.js
+++ src/components/products/ProductDetail.js
@@ -8,13 +8,15 @@ class ProductDetail extends Component{
   constructor(){
     super()
     this.state={
+
       anuncio:{
         // titulo:'Mi cochezaso',
         // descripcion:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         fotos:[],
         categorias:[],
         estado:[]
-      }
+      },
+      laFoto:''
     }
   }
 
@@ -22,12 +24,17 @@ class ProductDetail extends Component{
   componentWillMount(){
     firebase.database().ref('productos/' + this.props.match.params.productId)
     .on('value', (snap)=>{
-      this.setState({anuncio:snap.val()})
-      console.log(snap.val())
-    });
+      this.setState({anuncio:snap.val(), laFoto:snap.val().fotos[0]})
+      console.log(this.state)
+    })
   }
+
   sendMessage=()=>{
     message.success('Se ha enviado tu mensaje a Oswaldinho')
+  }
+
+  changeFoto=(foto)=>{
+    this.setState({laFoto:foto})
   }
   render(){
     return(
@@ -42,8 +49,19 @@ class ProductDetail extends Component{
                   )
                 })}</p>
               <div style={{width:'90%'}}>
-              <img style={{width:'90%'}} src={this.state.anuncio.fotos[0]}/>
-            </div>
+                <img style={{width:'90%'}} src={this.state.laFoto}/>
+              </div>
+              <div style={{width:'100px', display:'flex'}}>
+                {this.state.anuncio.fotos.map(f=>{
+                  return(
+                    <img
+                      src={f}
+                      style={{width:'100%', height:'100%', padding:'1%'}}
+                      onClick={()=>this.changeFoto(f)}/>
+                  )
+                })}
+              </div>
+
           </Col>
           <Col span={8}>
             <Card title="Oswaldinho" extra={<Avatar style={{marginTop:'-8px'}}src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />} style={{ width: 300 }}>
@@ -59,7 +77,7 @@ class ProductDetail extends Component{
                 size="large"
                 type="primary"
                 onClick={this.sendMessage}>Primary</Button>
-              <br/><br/><hr/>
+              <br/><br/><hr/><br/>
               <h3>Características:</h3>
               <p>{this.state.anuncio.descripcion}</p>
             </Card>
